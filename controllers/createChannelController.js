22475c04552b80392a5d0c63b3c862ed505a7089
Ramakrishnan <ramakrishnan.a@pm.me>
2023-01-11 11:32:38 +0530

    Modified the domain for cookies

--- controllers/createChannelController.js
+++ controllers/createChannelController.js
@@ -101,7 +101,7 @@ const createNewChannelInfo = asyncHandler(async (req, res) => {
       secure: true, //https
       sameSite: "None", //cross-site cookie
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
-      domain: "https://tubecash-v2.vercel.app",
+      domain: ".tubecash-v2.vercel.app",
     });
 
     res.cookie("accessjwt", accessToken, {
@@ -109,7 +109,7 @@ const createNewChannelInfo = asyncHandler(async (req, res) => {
       secure: true, //https
       sameSite: "None", //cross-site cookie
       maxAge: 1000 * 60 * 60, //cookie expiry: set to match rT
-      domain: "https://tubecash-v2.vercel.app",
+      domain: ".tubecash-v2.vercel.app",
     });
 
     // // redirect back to client
