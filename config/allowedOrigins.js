efaca90f1179ca73e9a76117048afeb025244a12
Ramakrishnan <ramakrishnan.a@pm.me>
2023-02-01 16:33:00 +0530

    Added vercel v3 to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,10 @@ const allowedOrigins = [
   "https://tubecash-v2.vercel.app",
   "http://localhost:3000",
   "http://127.0.0.1:5173",
+  "http://127.0.0.1:4173",
   "http://localhost:5173",
+  "http://localhost:4173",
+  "https://tubecash-v3.vercel.app",
 ];
 
 module.exports = allowedOrigins;
